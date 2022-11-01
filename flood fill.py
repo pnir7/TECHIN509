9232ad6857e0da8ea2f7e4fbd4f6d2ece6484df9
Anqi Pan <pananqi@MacBook-Pro-291.local>
2022-10-31 22:19:24 -0700

    try

--- flood fill.py
+++ flood fill.py
@@ -12,25 +12,21 @@ board = [
 ]
 
 
-def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
-    """Returns board with old values replaced with new values
-    through flood filling starting from the coordinates x, y
-    Args:
-        input_board (List[str])
-        old (str): Value to be replaced
-        new (str): Value that replaces the old
-        x (int): X-coordinate of the flood start point
-        y (int): Y-coordinate of the flood start point
-    Returns:
-        List[str]: Modified board
-    """
-    if 
-
-
+def flood_fill(input_board: list[str], old: str, new: str, x: int, y: int) -> list[str]:
+    for row in input_board:
+        col = list(row)
+        for i in col:
+            i = new
+            row = ''.join(col)
+        if x > 0 and x <= len(input_board) and y > 0 and y <= len(input_board[x]) and input_board[x][y] == old:
+        return input_board
+                
+ 
 modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
 
 for a in modified_board:
     print(a)
+
 
 # Expected output:
 # ......................
